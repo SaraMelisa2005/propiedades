215200f3fe9de4ece68d129b0823dc40125b1818
melisa pinilla <you@example.com>
2025-08-27 18:51:01 -0500

    js

---
+++ js/inicio.js
@@ -0,0 +1,67 @@
+console.log('hola desde java script');
+//console.error('ok 2 formato error se usa para identificar los errores en consola');
+/**variables:var:la mas antigua  let const:modernas*/
+let nombre="pepe";
+let apellido ="perez";
+let p ='1234';
+//forma 1 y mehor para concatenar
+const nombrecompleto= `${nombre} ${apellido}`;//ojo con el const siempre va a ser constante con let si se puede
+//forma 2 
+//nombrecompleto=nombre +''+apellido;
+//console.log(p) esto da error
+alert(nombrecompleto);//ventanas 
+let edad =25;//number
+let salario =25.5 //numbrer o float
+let mayorEdad =true;//Boolean
+let a =null;//nula
+let b =undefined;//variable no tiene valor exacto
+console.log(apellido)
+//array
+let numeros=[];//forma 1 array vacio
+numero =new Array(12);//forma 2 vacio pero se le asigna cantidad de elementos 
+numeros=[1,2,3,4,5,6,7,8,9,0];//forma 3 con los elementos del array
+let otro=['ashsh',12,12.5,true,[1,2,3]];//array con muchos tipos
+//objetos
+let persona ={
+    nombre:nombrecompleto,
+    apellido:'gomez',
+    edad:30,
+    mayorEdad:true,
+    jobs:[]
+
+};
+//manejo de array
+console.log (numeros[1])//posicion del elemento
+//manejo de objetos
+console.log(persona.nombre);//como el key 
+persona.nombre ='ana maria';
+console.log(persona.nombre,persona.apellido)
+//ciclos 
+console.log('ciclos.............')
+console.log('for -----------------')
+for(let index=0;index<numeros.length;index ++){
+    const mod=numeros[index]%2;
+    if(mod ==0){
+        console.log(numeros [index],'par');
+    }else{
+         console.log(numeros [index],'impar');
+    }
+}
+console.log('for in- no se ve la posicion----------------');
+for (let index in numeros){
+     const mod=numeros[index]%2;
+    if(mod ==0){
+        console.log(numeros [index],'par');
+    }else{
+         console.log(numeros [index],'impar');
+    }
+}
+console.log('for off- captura el valor----------------');
+for (let numero in numeros){
+     const mod=numeros %2;
+    if(mod ==0){
+        console.log(numero,'par');
+    }else{
+         console.log(numero,'impar');
+    }
+}
